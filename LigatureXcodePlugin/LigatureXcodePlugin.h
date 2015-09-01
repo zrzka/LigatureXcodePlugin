cf8793bb82ae6afd53ae7249c696576a9aaddcba
Robert Vojta <rvojta@me.com>
2015-09-01 09:13:07 +0200

    Initial revision

---
+++ LigatureXcodePlugin/LigatureXcodePlugin.h
@@ -0,0 +1,13 @@
+//
+//  LigatureXcodePlugin.h
+//  LigatureXcodePlugin
+//
+//  Created by Robert Vojta on 01.09.15.
+//  Copyright © 2015 Robert Vojta. All rights reserved.
+//
+
+@import Foundation;
+
+@interface LigatureXcodePlugin : NSObject
+
+@end
---
+++ LigatureXcodePlugin/LigatureXcodePlugin.m
@@ -0,0 +1,42 @@
+//
+//  LigatureXcodePlugin.m
+//  LigatureXcodePlugin
+//
+//  Created by Robert Vojta on 01.09.15.
+//  Copyright © 2015 Robert Vojta. All rights reserved.
+//
+
+#import "LigatureXcodePlugin.h"
+@import ObjectiveC.runtime;
+@import AppKit;
+
+@implementation LigatureXcodePlugin
+
+//
+// Ligatures
+//
+// <-  --  ->
+// ==  !=  !==
+// <=  >=  =>
+// ::
+// ++  +=  -= --
+//
+
++ (void)replaceAttributesAtLongestEffectiveRangeInRageForClass:(Class)clazz {
+  SEL selector = @selector(attributesAtIndex:longestEffectiveRange:inRange:);
+  Method m = class_getInstanceMethod(clazz, selector);
+  IMP oldImplementation = method_getImplementation(m);
+  IMP newImplementation = imp_implementationWithBlock((NSDictionary *)^(id self, NSUInteger location, NSRangePointer longestEffectiveRange, NSRange inRange) {
+    NSDictionary *attributes = ((NSDictionary * (*)(id, SEL, NSUInteger, NSRangePointer, NSRange))oldImplementation)(self, selector, location, longestEffectiveRange, inRange);
+    NSMutableDictionary *ma = [attributes mutableCopy];
+    [ma setObject:@1 forKey:NSLigatureAttributeName];
+    return ma;
+  });
+  method_setImplementation(m, newImplementation);
+}
+
++ (void)load {
+  [self replaceAttributesAtLongestEffectiveRangeInRageForClass:[NSAttributedString class]];
+}
+
+@end
